ca5d4d8f2ae69ffa5f8812e7c4c46d0e02bcd095
Laurie Young <laurie@wildfalcon.com>
2011-08-10 13:01:16 +0100

    Default rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5bootcamp::Application.config.secret_token = '3eb3de94bf5ca110e53b26d5f3aaccb685f9bebfd8b26fc0c3e2b8d63f3c042156bf17c16863ee7f486b574dcf69bd13fb332bcd0e60fce2d08756a4ef9f85e9'
